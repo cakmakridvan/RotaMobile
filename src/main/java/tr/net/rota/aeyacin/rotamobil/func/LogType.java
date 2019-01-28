d7aaef1808c1ad0574dc62b2fd7baef8530fb109
Rıdvan Çakmak <cakmak.ridvan@hotmail.com>
2019-01-28 09:38:58 +0300

    Revert "first commit"
    This reverts commit 3df7a48

--- src/main/java/tr/net/rota/aeyacin/rotamobil/func/LogType.java
+++
@@ -1,9 +0,0 @@
-package tr.net.rota.aeyacin.rotamobil.func;
-
-/**
- * Created by ayacin on 25.04.2017.
- */
-
-public enum LogType {
-
-}
--- src/main/java/tr/net/rota/aeyacin/rotamobil/model/sbt/VehicleState.java
+++
@@ -1,8 +0,0 @@
-package tr.net.rota.aeyacin.rotamobil.model.sbt;
-
-public class VehicleState {
-    public int VehicleId;
-    public String SentDateTime;
-    public boolean EngineBlockage;
-
-}
