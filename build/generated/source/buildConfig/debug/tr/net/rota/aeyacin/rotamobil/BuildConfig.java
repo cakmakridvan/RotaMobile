56e7593ba180f612022878c20407e830bb5240f5
Rıdvan Çakmak <cakmak.ridvan@hotmail.com>
2019-01-28 09:44:35 +0300

    committed

---
+++ build/generated/source/buildConfig/debug/tr/net/rota/aeyacin/rotamobil/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package tr.net.rota.aeyacin.rotamobil;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "tr.net.rota.aeyacin.rotamobil";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 32;
+  public static final String VERSION_NAME = "1.1.32";
+}
